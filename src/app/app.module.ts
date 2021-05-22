b19e162ff3ab9f968bca95a2fe4a124664c1cbef
Muhammed Shendi <muhammedshendi@Muhammeds-MacBook-Pro.local>
2021-05-23 01:49:33 +0200

    added product component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
-  imports: [BrowserModule, FontAwesomeModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductComponent,
+  ],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  @Input() product: any;
+  @Input() addToCart: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
