74160c3449a6e2a9dac6368fa69604560225aff7
Muhammed Shendi <muhammedshendi@Muhammeds-MacBook-Pro.local>
2021-05-26 05:52:41 +0200

    added products to shop component

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
-  selector: 'app-shop',
-  templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+    selector: 'app-shop',
+    templateUrl: './shop.component.html',
+    styleUrls: ['./shop.component.css'],
 })
 export class ShopComponent implements OnInit {
+    hero_title: string = 'Shop';
+    products: any = [];
+    constructor(private productService: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.productService.getProducts().subscribe(
+            (data) => (this.products = data),
+            (err) => console.error(err)
+        );
+    }
 }
